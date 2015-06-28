5ae0256c0097d3059101bd3ea01ff65a8b41b6aa
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2015-06-28 14:18:37 -0400

    legacy_events structure

---
+++ db/migrate/20150628171749_create_legacy_events.rb
@@ -0,0 +1,16 @@
+class CreateLegacyEvents < ActiveRecord::Migration
+  def change
+    create_table :legacy_events do |t|
+      t.string :case_label , null: false
+      t.string :counties , array: true, default: []
+      t.string :name, null: false
+      t.string :short_name, null: false
+      t.date :start_date, null: false
+      t.date :end_date, null: false
+      t.integer :num_sites
+      t.string :reminder_contents
+      t.integer :reminder_days
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/factories/incidents.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :incident do
+    
+  end
+
+end
