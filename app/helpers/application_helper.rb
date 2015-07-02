bbfdf50d79eb7c3834766a50d569bfe69a7f5d14
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2015-07-02 16:15:48 -0400

    admin namespacing routing and file structure

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def check_admin?
+        # here is a method to check if we are logged in
+        # if not...redirect
+    end
+    def is_super_admin?
+        # current_user.is_ccu_admin?
+    end
 end
