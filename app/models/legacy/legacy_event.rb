def9de2868503894aeee748879b49b079a7d42f3
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2015-07-07 16:10:43 -0400

    quick validations

--- app/models/legacy/legacy_event.rb
+++ app/models/legacy/legacy_event.rb
@@ -3,5 +3,7 @@ module Legacy
   	  has_many :legacy_organization_events
   	  has_many :legacy_sites
  	  has_many :legacy_organizations, through: :legacy_organization_events
+  	  validates_uniqueness_of :name,:short_name
+  	  validates_presence_of :name,:case_label,:created_date,:start_date,:end_date
   end
 end
