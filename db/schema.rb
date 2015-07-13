d474b7d0a9e8066582eaec519e8978d1c89783ea
Andy Gimma <andy.n.gimma@gmail.com>
2015-07-13 17:40:53 -0500

    sites ready

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150711112320) do
+ActiveRecord::Schema.define(version: 20150713213454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,6 +138,7 @@ ActiveRecord::Schema.define(version: 20150711112320) do
     t.datetime "updated_at",        null: false
     t.string   "appengine_key"
     t.date     "request_date"
+    t.date     "date_closed"
   end
 
 end
