826c8f84d095e73aedc5dfd69dd0ca70a3d76f6c
andygimma <andy.n.gimma@gmail.com>
2015-12-09 03:33:22 -0500

    More robust work_type param for spreadsheets api

--- app/controllers/api/spreadsheets_controller.rb
+++ app/controllers/api/spreadsheets_controller.rb
@@ -11,9 +11,7 @@ module Api
 			@sites = @sites.where('claimed_by is NOT NULL') if params[:claimed_by] == "true"
 			@sites = @sites.where("status LIKE '%Open%'") if params[:open] == "true"
 			@sites = @sites.where("status LIKE '%Closed%'") if params[:closed] == "true"
-			@sites = @sites.where("work_type LIKE '%Flood%'") if params[:work_type] == "flood"
-			@sites = @sites.where("work_type LIKE '%Goods%'") if params[:work_type] == "goods"
-			@sites = @sites.where("work_type LIKE '%Trees%'") if params[:work_type] == "trees"
+			@sites = @sites.where("work_type LIKE '%#{params[:work_type].titleize}%'") if params[:work_type]
 			@sites = @sites.where(legacy_event_id: params[:legacy_event_id].to_i) if is_i? params[:legacy_event_id]
 			@sites = @sites.where(claimed_by: params[:claimed_by].to_i) if is_i? params[:claimed_by]
 			send_data @sites.to_csv(params: params), :type => 'text/csv; charset=utf-8; header=present', :disposition => "attachment; filename=export_all.csv"
