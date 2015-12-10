957fee66296f3cfbe8cdc8692c57b54bfae344e3
andygimma <andy.n.gimma@gmail.com>
2015-12-10 15:01:39 -0500

    Make sure invitation passwords match. Notify user if invitation is accepted

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -8,14 +8,20 @@ class InvitationsController < ApplicationController
 
   def sign_up 
     inv = Invitation.where(token:params[:token]).first
-    
+    # make sure passwords match
+    if params["user"]["password"] != params["user"]["password_confirmation"]
+      flash[:notice] = "Passwords do not match"
+      redirect_to :back
+      return
+    end
     @user = User.new(email: params["user"]["email"],password: params["user"]["password"],name: params["user"]["name"], legacy_organization_id:inv.organization_id, referring_user_id:inv.user_id)
     if @user.save
       RequestInvitation.user_created!(params["user"]["email"])
       if inv.user.admin?
          @user.verify!
       end
-      redirect_to "/dashboard"
+      flash[:notice] = "Invitation accepted. Please log in."
+      redirect_to "/login"
     else
       redirect_to :back
     end  
