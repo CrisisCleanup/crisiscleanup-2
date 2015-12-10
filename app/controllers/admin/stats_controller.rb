e8f4285f7138be93d227f81f46ef756effda621f
Scott Golaszewski <golaszewski@visionlink.org>
2015-12-10 09:27:31 -0700

    WIP: stubbed stats export

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -1,26 +1,42 @@
 module Admin
   class StatsController < ApplicationController
+
     include ApplicationHelper
+
+    require 'csv'
+
     before_filter :check_admin?
+
     # add logic to only allow ccu admins to access this
     # before_filter :deny_access, :unless => :is_ccu_admin?
+
     def index
-    	@orgs_count = Legacy::LegacyOrganization.count 
-    	@events_count = Legacy::LegacyEvent.count
-    	@sites_count = Legacy::LegacySite.count
-    	@contacts_count = Legacy::LegacyContact.count
-        @users_count = User.count
-        @todays_login_count = User.todays_login_count
-        @todays_create_and_edit_count = Legacy::LegacySite.todays_create_and_edit_count
-        @events = Legacy::LegacyEvent.all
+      @orgs_count = Legacy::LegacyOrganization.count
+      @events_count = Legacy::LegacyEvent.count
+      @sites_count = Legacy::LegacySite.count
+      @contacts_count = Legacy::LegacyContact.count
+      @users_count = User.count
+      @todays_login_count = User.todays_login_count
+      @todays_create_and_edit_count = Legacy::LegacySite.todays_create_and_edit_count
+      @events = Legacy::LegacyEvent.all
     end
 
     def by_incident
-    	@event = Legacy::LegacyEvent.find(params[:id])
-    	org_ids = Legacy::LegacyOrganizationEvent.where(legacy_event_id: @event.id)
-    	@orgs = Legacy::LegacyOrganization.where(id: org_ids)
-    	@work_type_counts = Legacy::LegacySite.work_type_counts(@event.id)
-    	@status_counts = Legacy::LegacySite.status_counts(@event.id)
+      @event = Legacy::LegacyEvent.find(params[:id])
+      org_ids = Legacy::LegacyOrganizationEvent.where(legacy_event_id: @event.id)
+      @orgs = Legacy::LegacyOrganization.where(id: org_ids)
+      @work_type_counts = Legacy::LegacySite.work_type_counts(@event.id)
+      @status_counts = Legacy::LegacySite.status_counts(@event.id)
+      filename = Time.now.utc.iso8601.gsub('-', '').gsub(':', '') + "-" + @event.name + ".csv"
+
+      respond_to do |format|
+        format.html
+        format.csv do
+          headers['Content-Disposition'] = "attachment; filename=\"" + filename + "\""
+          headers['Content-Type'] ||= 'text/csv'
+        end
+      end
     end
+
   end
 end
