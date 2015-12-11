b39245ba439bbac53195524c75c61c99a0685156
Scott Golaszewski <golaszewski@visionlink.org>
2015-12-11 16:34:40 -0700

    Fix up the legacy factories to use some random enum data
    Partially layout stats export csv template

--- spec/factories/legacy_organization_event.rb
+++ spec/factories/legacy_organization_event.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :legacy_organization_event, :class => Legacy::LegacyOrganizationEvent do
-  	legacy_organization_id 1
-  	legacy_event_id 1                                 	
+    legacy_organization_id 1
+    legacy_event_id 1
   end
-end  
+end
 
