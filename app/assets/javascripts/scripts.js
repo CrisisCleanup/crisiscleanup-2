3139836769bf60ce607b943ed8ce7768d9d5727a
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2016-01-05 15:27:06 -0500

    loading gif and restructure of ccu map to make more sense

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,40 +1,34 @@
 
-$(document).on("ready page:load", function() {
+$(document).ready(function() {
 	
 	var path = $(location).attr('pathname');
 	var dashboard = path.split('/')[1] == 'admin';	
 	console.log(dashboard);
 	var page = path.split('/')[2];
 	var elem = '.'+page;
-	var workerMap = $('#map-canvas').length
+	var public_map = $('#public-map-canvas').length
+	var worker_map = $('#worker-map-canvas').length
 	
 	if (dashboard){
-		
 		$('select').foundationSelect();
-		
 		$('.dashboard li').removeClass('active');
 		$(elem).addClass('active');
 		if ($(elem).parents('.has-dropdown').length) {
 			$(elem).parents('.has-dropdown').addClass('active');
 		}
 	}
-	// if a map is on the page
-	// get incident id
-	if (workerMap != 0) {
-		var id, pin;
-		try {
-			var id = $('.m-id.hidden')[0].innerHTML;
-			// get pin to determine which pins, if any or all to display
-			var pin = $('.m-pin.hidden')[0].innerHTML;	
+	// if a map is on the page get incident id
+	if (public_map != 0 || worker_map != 0) {
+		// if no incident id show blank map
+		var id = typeof $('.m-id.hidden')[0] !== 'undefined' ? $('.m-id.hidden')[0].innerHTML : 'none';
+		// if no pin is specified it will show all pins
+		var pin = typeof $('.m-pin.hidden')[0] !== 'undefined' ? $('.m-pin.hidden')[0].innerHTML : 'all';
+		// if its a public map build it.
+		if (public_map != 0){
+			var ccmap = new CCMAP('public-map-canvas', 6);
+			google.maps.event.addDomListener(window,'ready',ccmap.buildPublicMap(pin));
 		}
-		catch(err) {
-		  
-		    console.log(err.message);
-		}
-
-		var ccmap = new CCMAP('map-canvas', id);
-	
-		google.maps.event.addDomListener(window, 'ready',ccmap.build(pin));
+		// to do worker 
 
 	}
 });
