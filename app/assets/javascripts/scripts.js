018d2c7ca22ac2eee82fa2d1ae43088dbff375e4
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2016-01-08 16:07:32 -0500

    cleaning up maps

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
 	console.log(dashboard);
 	var page = path.split('/')[2];
 	var elem = '.'+page;
-	var public_map = $('#public-map-canvas').length
 	var worker_map = $('#worker-map-canvas').length
 	
 	if (dashboard){
@@ -18,17 +17,10 @@ $(document).ready(function() {
 		}
 	}
 	// if a map is on the page get incident id
-	if (public_map != 0 || worker_map != 0) {
-		// if no incident id show blank map
+	if (worker_map != 0) {
+		
 		var id = typeof $('.m-id.hidden')[0] !== 'undefined' ? $('.m-id.hidden')[0].innerHTML : 'none';
-		// if no pin is specified it will show all pins
-		var pin = typeof $('.m-pin.hidden')[0] !== 'undefined' ? $('.m-pin.hidden')[0].innerHTML : 'all';
-		// if its a public map build it.
-		if (public_map != 0){
-			var ccmap = new CCMAP('public-map-canvas', 6);
-			google.maps.event.addDomListener(window,'ready',ccmap.buildPublicMap(pin));
-		}
-		// to do worker 
-
+		var ccmap = new CCMAP('worker-map-canvas', id);
+		ccmap.buildMarkers(id);
 	}
 });
--- app/assets/javascripts/worker/ccmap.js
+++ app/assets/javascripts/worker/ccmap.js
@@ -1,58 +1,63 @@
 // build map with all of the pins clustered
 var markers = [];
+var map;
+var markerCluster;
+var markerBounds = new google.maps.LatLngBounds();
 function CCMAP(elm, event_id,lat,lng,zoom){
 	this.canvas = document.getElementById(elm);
+	this.worker =  elm != 'public-map-canvas' ? true : false;
 	console.log(event_id);
 	this.incident = event_id;
 	this.zoom = typeof zoom !== 'undefined' ? zoom : 4;
 	this.latitude = typeof lat !== 'undefined' ? lat : 39;
    	this.longitude = typeof lng !== 'undefined' ? lng : -90;
-	
 	this.options = {
 	  center: new google.maps.LatLng(this.latitude, this.longitude),
 	  zoom: this.zoom,
 	  mapTypeId: google.maps.MapTypeId.ROADMAP,
 	  scrollwheel: false    
 	}
-
+	map = new google.maps.Map(this.canvas, this.options)
 }
 
-CCMAP.prototype.buildPublicMap = function(pin){
-	// first build empty map
-	$('.map-wrapper').prepend('<div class="loading"></div>')
-	var map = new google.maps.Map(this.canvas, this.options)
-	
-	if (this.incident != 'none' && pin == 'all'){
-		
-		$.ajax({
-		  	type: "GET",
-		  	url: "/api/public/map?legacy_event_id="+this.incident,
-		  	success: function(data){
+CCMAP.prototype.buildMarkers = function(id){
+	$('.map-wrapper').append('<div class="loading"></div>')
+	this.incident = id;
+	route = this.worker ? "/api/map?legacy_event_id="+id : "/api/public/map?legacy_event_id="+id
+	lat = this.worker ? "latitude" : "blurred_latitude";
+	lng = this.worker ? "longitude" : "blurred_longitude";
+	$.ajax({
+	  	type: "GET",
+	  	url: route,
+	  	success: function(data){
+			clearOverlays();
+			if (data.length > 0){
 				$.each(data, function(index, obj) {
+				 	markerBounds.extend(new google.maps.LatLng(parseFloat(obj[lat]), parseFloat(obj[lng])));
 			  		var marker = new google.maps.Marker({
-	          			position: new google.maps.LatLng(parseFloat(obj["blurred_latitude"]), parseFloat(obj["blurred_longitude"])),
+	          			position: new google.maps.LatLng(parseFloat(obj[lat]), parseFloat(obj[lng])),
 	            		map: map
 			        });
 			        markers.push(marker);
-
+			    
 			    })
-			    $('.loading').remove();
-	  		},
-	  		error: function(){
-	  			alert('500 error');
-	 		 }
-		})
-	}else{
-		$('.loading').remove();
-	}
-
-
-
+			    markerCluster = new MarkerClusterer(map, markers);
+			    map.fitBounds(markerBounds);
+				$('.loading').remove();    
+			}else{alert("no reported incidents");}			
+  		},
+  		error: function(){
+  			alert('500 error');
+ 		 }
+	})	
 }
-CCMAP.prototype.clearOverlays == function() {
-	if (markers.length > 0) {
-	  for (i in markers) {
-	    markers[i].setMap(null);
-	  }
+
+var clearOverlays = function() {
+	for (var i = 0; i < markers.length; i++) {
+		markers[i].setMap(map);
+	}
+	markers = [];
+	if (typeof markerCluster !== 'undefined'){
+		markerCluster.clearMarkers();
 	}
 }
