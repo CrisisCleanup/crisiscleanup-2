ed48596231f77b193bad1448f99020fb0e88df32
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2016-01-08 16:12:12 -0500

    putting back one migration;

---
+++ db/migrate/20151210063217_create_temporary_password.rb
@@ -0,0 +1,12 @@
+# class CreateTemporaryPassword < ActiveRecord::Migration
+  def change
+    create_table :temporary_passwords do |t|
+    	t.integer :created_by
+    	t.integer :legacy_organization_id
+    	t.string :password
+    	t.string :password_confirmation
+    	t.string :password_digest
+    	t.datetime :expires
+    end
+  end
+end
