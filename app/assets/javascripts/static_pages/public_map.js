a1a8b6c205668516e0197a9fe11ecbcb09603efe
Scott Golaszewski <golaszewski@visionlink.org>
2016-01-11 15:04:13 -0700

    Clean up some of the map js and add the map icon set

--- app/assets/javascripts/static_pages/public_map.js
+++ app/assets/javascripts/static_pages/public_map.js
@@ -1,15 +1,16 @@
-var ccmap;
-$(document).ready(function(){
-	var public_map = $('#public-map-canvas').length
-	if (public_map != 0){
-		var id = typeof $('.m-id.hidden')[0] !== 'undefined' ? $('.m-id.hidden')[0].innerHTML : 'none';
-		ccmap = new CCMAP('public-map-canvas', id);
-		
-		$( ".select_incident" ).change(function() {
-		    var event_id = $(".select_incident").val();
+$(document).ready(function() {
+  var public_map = $('#public-map-canvas').length
+  if (public_map != 0){
+    var id = typeof $('.m-id.hidden')[0] !== 'undefined' ? $('.m-id.hidden')[0].innerHTML : 'none';
+    var ccmap = new CCMAP({
+      elm: 'public-map-canvas',
+      event_id: id
+    });
 
-		    ccmap.buildMarkers(event_id);
-		});
-	}
+    $( ".select_incident" ).change(function() {
+        var event_id = $(".select_incident").val();
+
+        ccmap.buildMarkers(event_id);
+    });
+  }
 });
-	
