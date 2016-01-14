51da1c60ebeae2913eca2623dd2dc8ce4395ace6
Scott Golaszewski <sgola37@gmail.com>
2016-01-13 17:10:19 -0700

    WIP: break out map related objects

--- app/assets/javascripts/static_pages/public_map.js
+++ app/assets/javascripts/static_pages/public_map.js
@@ -2,15 +2,14 @@ $(document).ready(function() {
   var public_map = $('#public-map-canvas').length
   if (public_map != 0){
     var id = typeof $('.m-id.hidden')[0] !== 'undefined' ? $('.m-id.hidden')[0].innerHTML : 'none';
-    var ccmap = new CCMAP({
+    var ccmap = new CC.Map.Map({
       elm: 'public-map-canvas',
       event_id: id
     });
 
     $( ".select_incident" ).change(function() {
         var event_id = $(".select_incident").val();
-
-        ccmap.buildMarkers(event_id);
+        ccmap.setEventId(event_id);
     });
   }
 });
