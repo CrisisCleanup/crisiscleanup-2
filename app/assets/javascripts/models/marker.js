d2e41f83d315352a8ef04eddae9edf9239c66c63
Scott Golaszewski <golaszewski@visionlink.org>
2016-01-15 11:52:51 -0700

    WIP - stubbed map marker class file

---
+++ app/assets/javascripts/models/marker.js
@@ -0,0 +1,20 @@
+var CC = CC || {};
+CC.Map = CC.Map || {};
+
+// build map with all of the pins clustered
+var iconDir = '/assets/map_icons/';
+var unclaimedStatusColorMap = {
+  "Open, unassigned": "orange",
+  "Open, assigned": "yellow",
+  "Open, partially completed": "yellow",
+  "Open, partially completed": "yellow",
+  "Closed, completed": "green",
+  "Closed, incomplete": "gray",
+  "Closed, out of scope": "gray",
+  "Closed, done by others": "gray",
+  "Closed, no help wanted": "xgray",
+  "Closed, rejected": "xgray",
+  "Closed, duplicate": "xgray"
+};
+
+CC.Map.Marker = {};
