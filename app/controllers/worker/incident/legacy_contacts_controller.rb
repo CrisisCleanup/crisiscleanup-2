ef57a44559dd9c1e48d6c6020c2d2444a3426a08
Scott Golaszewski <sgola37@gmail.com>
2016-01-29 15:02:37 -0700

    LInk up nav to the proper routes
    If there's an issue with the db version in the db/schema.rb this is
    where it happened. I commented out the affected lines in legacy_contacts
    controller for now.

--- app/controllers/worker/incident/legacy_contacts_controller.rb
+++ app/controllers/worker/incident/legacy_contacts_controller.rb
@@ -1,23 +1,20 @@
 module Worker
-    module Incident
-      class LegacyContactsController < ApplicationController
-        include ApplicationHelper
-        before_filter :check_incident_permissions
-        
-        def index
-            org_ids = Legacy::LegacyOrganizationEvent.select("id").where(legacy_event_id: current_user_event)
-        	@contacts = Legacy::LegacyContact.order("first_name").where(legacy_organization_id: org_ids).paginate(:page => params[:page])
-            @event_id = params[:id]
-            @users = User.where(legacy_organization_id: current_user.legacy_organization_id)
-            binding.pry
-        end
+  module Incident
+    class LegacyContactsController < ApplicationController
+      include ApplicationHelper
+      before_filter :check_incident_permissions
 
-        def show
-        	@contact = Legacy::LegacyContact.find(params[:contact_id])
-            @event_id = params[:id]
-        end
-
+      def index
+        org_ids = Legacy::LegacyOrganizationEvent.select("id").where(legacy_event_id: current_user_event)
+        @contacts = Legacy::LegacyContact.order("first_name").where(legacy_organization_id: org_ids).paginate(:page => params[:page])
+        @event_id = params[:id]
+        @users = User.where(legacy_organization_id: current_user.legacy_organization_id)
       end
-end
 
+      def show
+        @contact = Legacy::LegacyContact.find(params[:contact_id])
+        @event_id = params[:id]
+      end
+    end
+  end
 end
