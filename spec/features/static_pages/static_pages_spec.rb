3b4c04c201703576ecefd0a31ffe22bdd7630a19
StraubCreative <zachary.a.straub@gmail.com>
2016-02-16 21:34:25 -0800

    rspec test

--- spec/features/static_pages/static_pages_spec.rb
+++ spec/features/static_pages/static_pages_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe "Going to the home page", :type => :feature do
   it "loads a page with the proper main header" do
     visit '/'
-    expect(page).to have_content 'Disaster Relief Coordination'
+    expect(page).to have_content 'You let them organize themselves'
   end
 end
