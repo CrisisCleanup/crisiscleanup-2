27aba3ffe602062b30483842d4383aeccd216424
andygimma <andy.n.gimma@gmail.com>
2016-02-24 06:07:52 +0100

    Form consolidation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,4 +12,22 @@ class StaticPagesController < ApplicationController
   end
   def terms
   end      
+
+  def signup
+  end
+
+  def new_incident
+  end
+
+  def request_incident
+    @users = User.where(admin:true)
+    @users.each do |user| 
+      InvitationMailer.send_incident_request(params, user.email)
+    end
+    flash[:notice] = "Your request has been received"
+    redirect_to "/"
+  end
+
+  def redeploy
+  end
 end
