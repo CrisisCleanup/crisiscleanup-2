64a01d5e13577c853b0de51c86fb7f2e13bc9ee3
andygimma <andy.n.gimma@gmail.com>
2016-02-27 08:58:15 +0100

    Fix typo

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,7 +5,6 @@ module Admin
     # add logic to only allow ccu admins to access this
     # before_filter :deny_access, :unless => :is_ccu_admin?
     def index
-    	binding.pry
     	@orgs = Legacy::LegacyOrganization.where(org_verified:false).order('created_at ASC')
     end
   end
