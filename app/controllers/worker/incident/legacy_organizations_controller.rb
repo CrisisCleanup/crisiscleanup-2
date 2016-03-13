3e4f32c5325718b6933f6e9f38ea01c739fa9bf0
andygimma <andy.n.gimma@gmail.com>
2016-03-13 12:31:03 +0100

    Switch back to current_user_event

--- app/controllers/worker/incident/legacy_organizations_controller.rb
+++ app/controllers/worker/incident/legacy_organizations_controller.rb
@@ -7,7 +7,7 @@ module Worker
       def index
         # search legacy_organization_event for legacy_organization_id: current_user_event
         # pass those ids to LegacyOrg.find(ids)
-        org_ids = Legacy::LegacyOrganizationEvent.select("legacy_organization_id").where(legacy_event_id: params[:id])
+        org_ids = Legacy::LegacyOrganizationEvent.select("legacy_organization_id").where(legacy_event_id: current_user_event)
         @orgs = Legacy::LegacyOrganization.order("name").where(id: org_ids).where(org_verified: true).paginate(:page => params[:page])
         @event_id = params[:id]
       end
