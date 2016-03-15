e1e86fb784eb0c3128725bb68ff2ff7a26bb44b8
Scott Golaszewski <golaszewski@visionlink.org>
2016-03-15 16:48:08 -0600

    Updated the email template

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -6,10 +6,11 @@ class InvitationMailer < ActionMailer::Base
     @url = request + "/invitations/activate?token="+inv.token
     mail(to: @email, subject: "#{@user.email} has invited you to join Crisis Cleanup")
   end
+
   def send_confirmation_alert(verified_by, new_user)
     @verified_by = verified_by
     @new_user = new_user
-   
+
     mail(to: @new_user.email, subject: "#{@verified_by.email} has granted you access")
   end
 
