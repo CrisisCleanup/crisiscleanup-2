7f5e77eec816c3d631f7ed3f086395068778a404
Scott Golaszewski <golaszewski@visionlink.org>
2016-03-15 13:48:34 -0600

    Fix the org signup notification emails

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,21 +3,21 @@ class AdminMailer < ActionMailer::Base
   def send_registration_alert(user, org)
     @user = user
     @org = org
-   
+
     mail(to: @user.email, subject: "#{@org.name} has registered for Crisis Cleanup")
   end
 
   def send_user_registration_alert(user, org)
-  	@user = user
-  	@org = org
+    @user = user
+    @org = org
     @name = nil
     if @org
       @name = @org.name
     else
       @name = "the Admin"
     end
-  	
-  	mail(to: @user.email, subject: "#{@user.name} has registered for Crisis Cleanup with #{@name}")
+
+    mail(to: @user.email, subject: "#{@user.name} has registered for Crisis Cleanup with #{@name}")
   end
- 	# TODO - do we need comment alerts? also an in-app mail or alert system, ala social network messages
+  # TODO - do we need comment alerts? also an in-app mail or alert system, ala social network messages
 end
