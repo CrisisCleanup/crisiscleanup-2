c699812f19119ba2767886411fc840623995f491
StraubCreative <zachary.a.straub@gmail.com>
2016-03-16 01:22:20 -0700

    fix capybara tests

--- spec/features/registrations/registrations_spec.rb
+++ spec/features/registrations/registrations_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 describe "Going to the signup page page", :type => :feature do
   it "has a New Organization Link" do
     visit '/signup'
-    click_link 'New Organization'
-    expect(page).to have_content 'Sign Up For Crisis Cleanup'
+    click_link 'Register Here'
+    expect(page).to have_content 'Organization Sign Up'
   end
 end
 
 describe "Going to the signup page page", :type => :feature do
   it "has a New Organization Link" do
     visit '/signup'
-    click_link 'I Have a Temporary Password'
+    click_link 'Use Password'
     expect(page).to have_content 'Use a temporary password'
   end
 end
@@ -19,7 +19,7 @@ end
 describe "Going to the signup page page", :type => :feature do
   it "has a New Organization Link" do
     visit '/signup'
-    click_link 'New Incident'
+    click_link 'Register Now'
     expect(page).to have_content 'Request to use CrisisCleanup for a new incident.'
   end
 end
@@ -27,7 +27,7 @@ end
 describe "Going to the signup page page", :type => :feature do
   it "has a New Organization Link" do
     visit '/signup'
-    click_link 'Redeploy Your Organization'
+    click_link 'Redeploy Now'
     expect(page).to have_content 'Redeploy to a new incident.'
   end
 end
@@ -35,7 +35,7 @@ end
 describe "Going to the signup page page", :type => :feature do
   it "has a New Organization Link" do
     visit '/signup'
-    click_link 'Just Curious'
+    click_link 'Learn More'
     expect(page).to have_content 'The People and Philosophies of Crisis Cleanup'
   end
 end
