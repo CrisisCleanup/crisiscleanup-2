c1d4014ba11ec45b64c2991c2bfe88b286570c89
Scott Golaszewski <golaszewski@visionlink.org>
2016-03-16 11:58:01 -0600

    Remove the metaphone headers from the sites csv download

--- app/models/legacy/legacy_event.rb
+++ app/models/legacy/legacy_event.rb
@@ -8,7 +8,7 @@ module Legacy
     has_many :legacy_organizations, through: :legacy_organization_events
     has_one :form,
       inverse_of: :legacy_event
-    validates_presence_of :name,:case_label,:created_date,:start_date
+    validates_presence_of :name, :case_label, :created_date, :start_date
 
     CASE_LABELS = %w(A B C D E F G H J K M N Q R S T U V W X Y Z)
     def self.next_case_label
@@ -18,8 +18,13 @@ module Legacy
     def sites_to_csv
       if legacy_sites.size > 0
         CSV.generate(headers: true) do |csv|
-          csv << legacy_sites[0].attribute_names
+          # Generate the header
+          header = legacy_sites[0].attribute_names.select do |attr|
+            !["name_metaphone", "city_metaphone", "county_metaphone", "address_metaphone"].include? attr
+          end
+          csv << header
 
+          # Generate the data rows
           legacy_sites.each do |site|
             csv << site.attributes.values
           end
