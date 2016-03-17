c090c6ec4c8283179dbd42dbe8543ff800ccc78a
Scott Golaszewski <sgola37@gmail.com>
2016-03-17 12:45:57 -0600

    WIP - form updates

--- app/controllers/admin/legacy_contacts_controller.rb
+++ app/controllers/admin/legacy_contacts_controller.rb
@@ -5,43 +5,50 @@ module Admin
     # add logic to only allow ccu admins to access this
     # before_filter :deny_access, :unless => :is_ccu_admin?
     def index
-        # todo implement search and sort and paginate
-        @contacts = Legacy::LegacyContact.order("first_name").paginate(:page => params[:page])
+      # todo implement search and sort and paginate
+      @contacts = Legacy::LegacyContact.order("first_name").paginate(:page => params[:page])
     end
     def new
-    	@contact = Legacy::LegacyContact.new
+      @contact = Legacy::LegacyContact.new
     end
-    def create    
-        @contact = Legacy::LegacyContact.new(contact_params) 
-        if @contact.save
-            flash[:notice] = "Contact #{@contact.first_name} #{@contact.last_name} successfully saved"
-         	redirect_to admin_legacy_contacts_path
-        else
-            flash[:alert] = "Contact #{@contact.first_name} #{@contact.last_name} not saved"
-            render :new
-        end
-    
+    def create
+      @contact = Legacy::LegacyContact.new(contact_params)
+      if @contact.save
+        flash[:notice] = "Contact #{@contact.first_name} #{@contact.last_name} successfully saved"
+        redirect_to admin_legacy_contacts_path
+      else
+        flash[:alert] = "Contact #{@contact.first_name} #{@contact.last_name} not saved"
+        render :new
+      end
+
     end
     def edit
-        @contact = Legacy::LegacyContact.find(params[:id])
-        @versions = @contact.versions
+      @contact = Legacy::LegacyContact.find(params[:id])
+      @versions = @contact.versions
     end
 
     def update
-        @contact = Legacy::LegacyContact.find(params[:id])
-        if  @contact.update_attributes(contact_params)
-            flash[:notice] = "Contact #{@contact.first_name} #{@contact.last_name} successfully updated"
-            redirect_to admin_legacy_contacts_path
-        else
-            flash[:alert] = "Contact #{@contact.first_name} #{@contact.last_name} not updated"
-            redirect_to edit_admin_legacy_contact_path(@contact.id)
-        end
+      @contact = Legacy::LegacyContact.find(params[:id])
+      if  @contact.update_attributes(contact_params)
+        flash[:notice] = "Contact #{@contact.first_name} #{@contact.last_name} successfully updated"
+        redirect_to admin_legacy_contacts_path
+      else
+        flash[:alert] = "Contact #{@contact.first_name} #{@contact.last_name} not updated"
+        redirect_to edit_admin_legacy_contact_path(@contact.id)
+      end
     end
-    
+
     private
-	    def contact_params
-	        params.require(:legacy_legacy_contact).permit(:email,:first_name,:last_name,:legacy_organization_id,:is_primary,:phone)
-	    end
+    def contact_params
+      params.require(:legacy_legacy_contact)
+        .permit(:email,
+                :first_name,
+                :last_name,
+                :legacy_organization_id,
+                :organizational_title,
+                :is_primary,
+                :phone)
+    end
   end
 end
 
--- app/models/legacy/legacy_contact.rb
+++ app/models/legacy/legacy_contact.rb
@@ -1,10 +1,10 @@
 module Legacy
   class LegacyContact < ActiveRecord::Base
-  	  self.per_page = 500
-  	  has_paper_trail
-  	  belongs_to :legacy_organization,
-    	inverse_of: :legacy_contacts
-    validates_presence_of :email, :first_name, :last_name, :phone 
+    self.per_page = 500
+    has_paper_trail
+    belongs_to :legacy_organization,
+      inverse_of: :legacy_contacts
+    validates_presence_of :email, :first_name, :last_name, :phone
     # validates_uniqueness_of :email
 
     def display_name
