0c5f75b04faf5e001f1cad5be46dfdc68621be21
andygimma <andy.n.gimma@gmail.com>
2016-03-25 21:30:29 +0100

    display the correct contacts by incident

--- app/controllers/worker/incident/legacy_contacts_controller.rb
+++ app/controllers/worker/incident/legacy_contacts_controller.rb
@@ -5,7 +5,7 @@ module Worker
       before_filter :check_incident_permissions
 
       def index
-        org_ids = Legacy::LegacyOrganizationEvent.select("legacy_organization_id").where(legacy_event_id: current_user_event)
+        org_ids = Legacy::LegacyOrganizationEvent.where(legacy_event_id: current_user_event).pluck(:legacy_organization_id)
         @contacts = Legacy::LegacyContact.order("first_name").where(legacy_organization_id: org_ids).paginate(:page => params[:page])
         @event_id = params[:id]
         @users = User.where(legacy_organization_id: current_user.legacy_organization_id)
