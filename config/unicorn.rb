d46e9ef56d72a5da305b3101a51aefe8d014fcb3
Scott Golaszewski <golaszewski@visionlink.org>
2016-04-22 09:24:42 -0600

    Add a dev config for the unicorn web server that allows 10000 seconds timeout.
    This will allow pry sessions without timing out in the middle.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,14 @@
 # config/unicorn.rb
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 15
-preload_app true
+
+# Allows the use of pry with unicorn in development.
+if ENV["RAILS_ENV"] == "development"
+  worker_processes 1
+  timeout 10000
+else
+  worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+  timeout 15
+  preload_app true
+end
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
