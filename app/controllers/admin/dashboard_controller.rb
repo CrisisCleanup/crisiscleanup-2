70d97a5ddcfa105dd87424a0564bb4ed6ee3cc7c
Scott Silvertein <scsilver.umd@gmail.com>
2016-06-12 16:15:37 -0600

    Change orgs order in dashboard_ctrl to by name

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,7 +5,7 @@ module Admin
     # add logic to only allow ccu admins to access this
     # before_filter :deny_access, :unless => :is_ccu_admin?
     def index
-    	@orgs = Legacy::LegacyOrganization.where(org_verified:false).order('created_at DESC')
+    	@orgs = Legacy::LegacyOrganization.where(org_verified:false).order('name')
     end
   end
 end
