575d8a12123e2fe8a537d5b23714bf4b8d9ec965
Scott Silvertein <scsilver.umd@gmail.com>
2016-06-12 17:06:55 -0600

    Add test for dashboard controller index to sort org name by alpha

--- spec/controllers/admin/dashboard_controller_spec.rb
+++ spec/controllers/admin/dashboard_controller_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe Admin::DashboardController, :type => :controller do
 
 
 	before do |example|
-	  org = FactoryGirl.create :legacy_organization 
+	  org = FactoryGirl.create(:legacy_organization, name: 'beta')
+	  org2 = FactoryGirl.create(:legacy_organization, name: 'alpha') 
 	  @admin = User.create(name:'Frank', email:'Frank@aol.com', password:'blue32blue32', legacy_organization_id: org.id, admin: true) 
 	  @user = User.create(name:'Gary', email:'Gary@aol.com', password:'blue32blue32', legacy_organization_id: org.id, admin: false) 
 	end
@@ -17,6 +18,15 @@ RSpec.describe Admin::DashboardController, :type => :controller do
 				get :index
 				expect(should).to render_template :index
 			end
+
+			it "returns organizations in alphabetical order" do
+				allow(controller).to receive(:current_user).and_return(@admin)
+				get :index
+				assigns(:orgs).map{|org| org.name}.should eq(['alpha','beta'])
+			end
+
+
+
 		end
 
 		context "without a current_user" do
