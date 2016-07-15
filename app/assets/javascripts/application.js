334dfb1210c29b0498efefde5742a18f22b8b31e
Aaron Titus <titus@tituss-Macbook-PRO-3.local>
2016-07-15 11:17:11 -0600

    New Icons, recompile JS
    added a comment to application.js to try to get the JS to reload, which
    gets new icons to appear on the map.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,8 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
+// CHANGE THIS LINE TO RECOMPILE JAVASCRIPT, TO GET NEW ICONS TO LOAD. 2016-07-15
+//
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
