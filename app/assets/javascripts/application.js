a50df05942cfc405749acce1009ec45bad3c3711
Aaron Titus <titus@tituss-Macbook-PRO-3.local>
2016-07-31 09:30:22 -0600

    Attempt to recompile JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,8 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-// CHANGE THIS LINE TO RECOMPILE JAVASCRIPT, TO GET NEW ICONS TO LOAD. 2016-07-29 4:12 pm
+// CHANGE THIS LINE TO RECOMPILE JAVASCRIPT, TO GET NEW ICONS TO LOAD. 2016-07-31 9:29 am
+// 
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
