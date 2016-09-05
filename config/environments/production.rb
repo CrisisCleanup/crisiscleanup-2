8bc7a0eea49f6664851040c5d23932fc0bb0ac4f
Aaron Titus <aaron@crisiscleanup.org>
2016-09-04 23:19:58 -0600

    Trying to fix end again.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,6 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-end
-if ENV["MEMCACHEDCLOUD_SERVERS"]
-    config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(','), { :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"] }
+  config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(','), { :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"] }
+
 end
