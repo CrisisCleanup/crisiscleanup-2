fea313ddaa3ec6ef1fee7e1f5300392f86a07e9c
Aaron Titus <aaron@crisiscleanup.org>
2016-12-21 16:52:44 -0700

    Comment out New Incident, update Rspec

--- spec/features/registrations/registrations_spec.rb
+++ spec/features/registrations/registrations_spec.rb
@@ -9,7 +9,7 @@ describe "Going to the signup page page", :type => :feature do
 end
 
 describe "Going to the signup page page", :type => :feature do
-  it "has a New Organization Link" do
+  it "has a Use Temporary Password Link" do
     visit '/signup'
     click_link 'Use Password'
     expect(page).to have_content 'Use a temporary password'
@@ -17,23 +17,7 @@ describe "Going to the signup page page", :type => :feature do
 end
 
 describe "Going to the signup page page", :type => :feature do
-  it "has a New Incident Link" do
-    visit '/signup'
-    click_link 'Register Now'
-    expect(page).to have_content 'Request to use CrisisCleanup for a new incident'
-  end
-end
-
-describe "Going to the signup page page", :type => :feature do
-  it "has a New Organization Link" do
-    visit '/signup'
-    click_link 'Redeploy Now'
-    expect(page).to have_content 'Redeploy to a new incident'
-  end
-end
-
-describe "Going to the signup page page", :type => :feature do
-  it "has a New Organization Link" do
+  it "has a Just Curious Link" do
     visit '/signup'
     click_link 'Learn More'
     expect(page).to have_content 'The People and Philosophies of Crisis Cleanup'
