40fa72c0aac6c9b84c2a0e8623fb6c9bd5dc1fd2
Tyler Sanders <tylerssanders@gmail.com>
2017-09-04 06:33:43 -0500

    Page objects

--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -16,12 +16,14 @@ module Features
       event = FactoryGirl.create(:legacy_event)
       org.legacy_events << event
       #org_event = FactoryGirl.create(:legacy_organization_event, legacy_organization_id: org.id, legacy_event_id: 1)
-	    FactoryGirl.create(:user, email: "Gary@aol.com", password: "blue32blue32", legacy_organization_id: org.id)
+      email = "Gary@aol.com"
+	    FactoryGirl.create(:user, email: email, password: "blue32blue32", legacy_organization_id: org.id)
 
       visit "/login"
-      fill_in 'Email', with: "Gary@aol.com"
+      fill_in 'Email', with: email
       fill_in 'Password', with: "blue32blue32"
       click_button 'Log in'
+      email
     end
 
     def sign_in_with(email, password)
@@ -34,6 +36,11 @@ module Features
     def send_invites(email_addresses)
     	fill_in 'email_addresses', with: email_addresses
     	click_button 'Submit Invites'
+    end
+
+    def create_incident
+      org = FactoryGirl.create(:legacy_organization)
+	    org_event = FactoryGirl.create(:legacy_organization_event, legacy_organization_id: org.id)
     end
   end
 end
