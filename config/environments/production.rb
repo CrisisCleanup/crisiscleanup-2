0da7ffa2d39e59cc5ed8d8f2a75460dc8dd5060b
Tyler Sanders <tylerssanders@gmail.com>
2017-09-12 23:53:54 -0500

    Revert "Update sendgrid settings"
    This reverts commit 2f5a7c031524a72f0fb273b54b80f52d89d5a522.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,18 +13,16 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'crisiscleanup.org',
-  :address        => 'smtp.sendgrid.net',
-  :port           => 587,
-  :authentication => :plain,
+  :domain         => 'heroku.com',
   :enable_starttls_auto => true
   }
   config.action_mailer.default_url_options = { :host => 'crisiscleanup.org' }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
