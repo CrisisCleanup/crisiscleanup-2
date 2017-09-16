800129d39def3e9aa8ec4cb270f0bf55f363fc96
Jacob Schooley <jacob.schooley@gmail.com>
2017-09-16 16:26:54 -0400

    Add button to move maps to user's location. Tested and working locally with Resource Override in Chrome.

--- app/assets/javascripts/models/map.js
+++ app/assets/javascripts/models/map.js
@@ -448,4 +448,74 @@ CCMap.Map = function(params) {
 
     }
   }
+  
+  // Geolocation
+  var defaultRandomLocation = {lat:31.4181, lng:73.0776};
+  var myLocationMarker = new google.maps.Marker({
+    map: this.map,
+    animation: google.maps.Animation.DROP,
+    position: defaultRandomLocation,
+    visible: false
+  });
+  function addLocationButton(map, marker)
+  {
+    var controlDiv = document.createElement('div');
+    
+    var firstChild = document.createElement('button');
+    firstChild.style.backgroundColor = '#fff';
+    firstChild.style.border = 'none';
+    firstChild.style.outline = 'none';
+    firstChild.style.width = '28px';
+    firstChild.style.height = '28px';
+    firstChild.style.borderRadius = '2px';
+    firstChild.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
+    firstChild.style.cursor = 'pointer';
+    firstChild.style.marginRight = '10px';
+    firstChild.style.padding = '0px';
+    firstChild.title = 'Your Location';
+    controlDiv.appendChild(firstChild);
+    
+    var secondChild = document.createElement('div');
+    secondChild.style.margin = '5px';
+    secondChild.style.width = '18px';
+    secondChild.style.height = '18px';
+    secondChild.style.backgroundImage = 'url(https://maps.gstatic.com/tactile/mylocation/mylocation-sprite-1x.png)';
+    secondChild.style.backgroundSize = '180px 18px';
+    secondChild.style.backgroundPosition = '0px 0px';
+    secondChild.style.backgroundRepeat = 'no-repeat';
+    secondChild.id = 'you_location_img';
+    firstChild.appendChild(secondChild);
+    
+    google.maps.event.addListener(map, 'dragend', function() {
+      $('#you_location_img').css('background-position', '0px 0px');
+    });
+    
+    firstChild.addEventListener('click', function() {
+      var imgX = '0';
+      var animationInterval = setInterval(function(){
+        if(imgX == '-18') imgX = '0';
+        else imgX = '-18';
+        $('#you_location_img').css('background-position', imgX+'px 0px');
+      }, 500);
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          marker.setPosition(latlng);
+          marker.setVisible(true);
+          map.setCenter(latlng);
+          map.setZoom(14);
+          clearInterval(animationInterval);
+          $('#you_location_img').css('background-position', '-144px 0px');
+        });
+      }
+      else{
+        clearInterval(animationInterval);
+        $('#you_location_img').css('background-position', '0px 0px');
+      }
+    });
+    
+    controlDiv.index = 1;
+    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(controlDiv);
+  }
+  addLocationButton(this.map, myLocationMarker);
 }
