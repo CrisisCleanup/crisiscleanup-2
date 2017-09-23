be6d1aa161010dac2d66430c5b8fdea36996699a
Anton Astashov <anton.astashov@gmail.com>
2017-09-22 21:31:23 -0500

    Add index on the `users.is_disabled` field

--- db/migrate/20170922013939_add_is_disabled_to_users.rb
+++ db/migrate/20170922013939_add_is_disabled_to_users.rb
@@ -1,5 +1,6 @@
 class AddIsDisabledToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :is_disabled, :boolean, default: false
+    add_column :users, :is_disabled, :boolean, default: false, null: false
+    add_index :users, :is_disabled
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -233,10 +233,11 @@ ActiveRecord::Schema.define(version: 20170922013939) do
     t.boolean  "accepted_terms"
     t.datetime "accepted_terms_timestamp"
     t.string   "title"
-    t.boolean  "is_disabled",              default: false
+    t.boolean  "is_disabled",              default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["is_disabled"], name: "index_users_on_is_disabled", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "versions", force: :cascade do |t|
