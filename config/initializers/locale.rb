de514e9b2acda2145344a09f453c61605da6aa22
Tyler Sanders <tylerssanders@gmail.com>
2017-10-09 23:11:45 -0500

    CrisisCleanup now loves español!

---
+++ config/initializers/locale.rb
@@ -0,0 +1,10 @@
+# config/initializers/locale.rb
+
+# Where the I18n library should search for translation files
+I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+
+# Whitelist locales available for the application
+I18n.available_locales = [:en, :es]
+
+# Set default locale to something other than :en
+I18n.default_locale = :es
