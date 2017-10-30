71711a312df2cfe3b55a0bedbebc8f024182be9a
Tyler Sanders <tylerssanders@gmail.com>
2017-10-29 21:22:38 -0500

    Autodetect language

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -6,4 +6,4 @@ I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
 # Whitelist locales available for the application
 I18n.available_locales = [:en, :es]
 
-I18n.default_locale = :es
+I18n.default_locale = :en
