d784a687b972b0fd8497241991ae76a0cfb53401
Tyler Sanders <tylerssanders@gmail.com>
2018-09-10 00:18:58 +0000

    Authentication and link

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,18 +1,10 @@
 RailsAdmin.config do |config|
 
-  ### Popular gems integration
-
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == Cancan ==
-  # config.authorize_with :cancan
-
-  ## == Pundit ==
-  # config.authorize_with :pundit
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
   ## == PaperTrail ==
   # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
