3f4729fd8fe3cacf1ec0c3076b0e238c89ba3163
Tyler Sanders <tylerssanders@gmail.com>
2018-10-14 19:41:32 +0000

    Update contrib docs and add cloud9 setup script

---
+++ cloud9_setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+# docker-compose setup
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
