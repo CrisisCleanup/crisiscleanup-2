49ead7af45dc6f0874f1ec848a5495b4b4e2bf05
Tyler Sanders <tylerssanders@gmail.com>
2019-09-06 02:27:04 +0000

    csv_generator_job test coverage

--- spec/jobs/csv_generator_job_spec.rb
+++ spec/jobs/csv_generator_job_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe CsvGeneratorJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "CSV generator - perform" do
+    it "enqueues a job" do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        CsvGeneratorJob.perform_later('generate_sites', 'TEST', 'somefile', 'test-bucket')
+      }.to have_enqueued_job
+    end
+    
+    let(:csv_doc) { IO.read(Rails.root.join("spec", "fixtures", "test.csv")) }
+    it "performs basic execution with params" do
+      allow_any_instance_of(CsvGeneratorJob).to receive(:generate_sites_csv).and_return(nil)
+      allow_any_instance_of(CsvGeneratorJob).to receive(:job_id).and_return('632b7c50')
+      expected_file_basename = 'TEST-632b7c50.csv'
+      expected_path = '/tmp/TEST-632b7c50.csv'
+      allow_any_instance_of(S3Helper).to(
+        receive(:upload_to_s3).with(expected_file_basename, 'somefile.txt', expected_path).and_return(nil)
+      )
+      CsvGeneratorJob.perform_now('generate_sites', 'TEST', 'somefile.txt', 'test-bucket')
+    end
+    
+    it "will catch S3Helper exception" do
+      allow_any_instance_of(CsvGeneratorJob).to receive(:generate_sites_csv).and_return(nil)
+      allow_any_instance_of(CsvGeneratorJob).to receive(:job_id).and_return('632b7c50')
+      expected_file_basename = 'TEST-632b7c50.csv'
+      expected_path = '/tmp/TEST-632b7c50.csv'
+      allow_any_instance_of(S3Helper).to(
+        receive(:upload_to_s3).with(expected_file_basename, 'somefile.txt', expected_path).and_raise('No S3 upload')
+      )
+      CsvGeneratorJob.perform_now('generate_sites', 'TEST', 'somefile.txt', 'test-bucket')
+    end   
+  end
+  
+  describe "generate_sites_csv" do
+    before do |example|
+      FactoryGirl.create :legacy_event 
+    end
+    
+    it "basic" do
+      ActiveJob::Base.queue_adapter = :test
+      event = Legacy::LegacyEvent.first
+	 		org = FactoryGirl.create :legacy_organization 
+		 	site1 = FactoryGirl.create :legacy_site, reported_by: org.id, name: 'John Doe'
+		 	
+	    site1.claimed_by = org.id
+      cgj = CsvGeneratorJob.new
+      
+      csv_store = []
+      cgj.generate_sites_csv(event.id, org.id).each { |element| csv_store << element }
+    end
+  end
 end
